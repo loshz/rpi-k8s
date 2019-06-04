f988b0cfaecda3eab0d1a27451e0529339a18b04
Dan Bond <pm@danbond.io>
2019-06-04 17:08:43 +0100

    remove worker script

--- scripts/bin/master.sh
+++ scripts/bin/master.sh
@@ -12,6 +12,7 @@ sudo kubeadm init --token-ttl=0 --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl
 
 # Install Flannel CNI
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
